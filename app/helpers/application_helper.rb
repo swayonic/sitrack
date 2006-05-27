4ac51b560b6fe4034572ff28fc9e12c248663b8d
josh.starcher <josh.starcher@74a96b24-c814-0410-a507-a4a592a6498a>
2006-05-27 17:14:56 +0000

    git-svn-id: http://svn.uscm.org/sitrack/trunk@3 74a96b24-c814-0410-a507-a4a592a6498a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,20 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def display_option_box(options, prefix='')
+    ret_val = ''
+    options.each do |key, value|
+      key = prefix+key
+      ret_val += "<div class=\"select_item\" onclick=\"check(document.search_f['#{key}']);\"><input type=\"Checkbox\" name=\"#{key}\" value=\"#{value}\" title=\"#{value}\" onclick=\"check(document.search_f['#{key}']);\" $check> #{value}<br /></div>\n";
+    end
+    return ret_val
+  end
+  
+  def app_years
+  	years = []
+    (2003..Time.now.year).sort{|a,b| b<=>a}.each do |year|
+      years << ['y'+year.to_s,year]
+  	end
+  	return years
+  end
 end
