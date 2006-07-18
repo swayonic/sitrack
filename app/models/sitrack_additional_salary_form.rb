ee9270561481dc022fc5d930b8ba9dd1a0b5b4b0
josh.starcher <josh.starcher@74a96b24-c814-0410-a507-a4a592a6498a>
2006-07-18 17:34:27 +0000

    the "form type" was wrong
    git-svn-id: http://svn.uscm.org/sitrack/trunk@49 74a96b24-c814-0410-a507-a4a592a6498a

--- app/models/sitrack_additional_salary_form.rb
+++ app/models/sitrack_additional_salary_form.rb
@@ -1,7 +1,7 @@
 class SitrackAdditionalSalaryForm < SitrackForm
   def email(var_hash, form_html)
     to = 'Josh Starcher <josh.starcher@uscm.org>'
-    email = FormMailer.create_form_email(to, var_hash, form_html, 'Salary Form')
+    email = FormMailer.create_form_email(to, var_hash, form_html, 'Additional Salary Form')
     FormMailer.deliver(email)
     
     # Stamp "form submitted" column
