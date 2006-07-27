806b0c5435c4c44fd36206a492e1d2c315f78a48
josh.starcher <josh.starcher@74a96b24-c814-0410-a507-a4a592a6498a>
2006-07-27 20:04:05 +0000

    This was a waste of my day
    git-svn-id: http://svn.uscm.org/sitrack/trunk@119 74a96b24-c814-0410-a507-a4a592a6498a

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,13 +37,13 @@ module ApplicationHelper
     
   # get the name of a project
   def get_project(id)
-    project(id)
+    self.project(id)
   end
   def self.get_project(id)
-    project(id)
+    self.project(id)
   end
   
-  def project(id)
+  def self.project(id)
     return '' if  0 == id || id.nil? || '' == id.to_s.strip # an id of 0 is useless. you're useless too
     session[:projects] ||= []
     # early return if we have this project name cached
