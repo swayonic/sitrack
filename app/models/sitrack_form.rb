662ab838dbac2318dc8886afeed4b8db65d4220c
josh.starcher <josh.starcher@74a96b24-c814-0410-a507-a4a592a6498a>
2006-08-23 13:46:06 +0000

    added 'enforce schema rules' to forms
    git-svn-id: http://svn.uscm.org/sitrack/trunk@150 74a96b24-c814-0410-a507-a4a592a6498a

--- app/models/sitrack_form.rb
+++ app/models/sitrack_form.rb
@@ -45,7 +45,8 @@
 
 class SitrackForm < ActiveRecord::Base
   belongs_to :hr_si_application
-  belongs_to :approver
+  belongs_to :approver, :class_name => "Person"
+  enforce_schema_rules
   
   def to
     if hr_si_application.sitrack_tracking.is_stint?
