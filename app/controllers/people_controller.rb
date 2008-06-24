e46ec23a48e58a73de339edc56fa65f7e96e976d
justin.sabelko <justin.sabelko@74a96b24-c814-0410-a507-a4a592a6498a>
2008-06-24 22:05:41 +0000

    PeopleController#create should only respond to POSTs
    git-svn-id: http://svn.uscm.org/sitrack/trunk@228 74a96b24-c814-0410-a507-a4a592a6498a

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -9,47 +9,51 @@ class PeopleController < ApplicationController
   end
   
   def create
-    @person = Person.new(params[:person])
-    params[:current_address][:addressType] = "current"
-    @current_address = CurrentAddress.new(params[:current_address])
-    respond_to do |format|
-      if @person.valid? && @current_address.valid?
-        # ===============================================
-        # = Check for duplicate person. VERY IMPORTANT! =
-        # ===============================================
-        exact = Person.find_exact(@person, @current_address)
-        # similar = Person.find_similar(@person, @current_address)
-        if exact
-          # Get rid of empty values
-          params[:person].delete_if {|key, value| value.strip == "" } 
-          params[:current_address].delete_if {|key, value| value.strip == "" } 
-          exact.update_attributes(params[:person])
-          @person = exact
-          @person.current_address ||= @current_address
-          @person.current_address.update_attributes(params[:current_address])
-          @current_address = @person.current_address
-        else
+    if request.post?
+      @person = Person.new(params[:person])
+      params[:current_address][:addressType] = "current"
+      @current_address = CurrentAddress.new(params[:current_address])
+      respond_to do |format|
+        if @person.valid? && @current_address.valid?
+          # ===============================================
+          # = Check for duplicate person. VERY IMPORTANT! =
+          # ===============================================
+          exact = Person.find_exact(@person, @current_address)
+          # similar = Person.find_similar(@person, @current_address)
+          if exact
+            # Get rid of empty values
+            params[:person].delete_if {|key, value| value.strip == "" } 
+            params[:current_address].delete_if {|key, value| value.strip == "" } 
+            exact.update_attributes(params[:person])
+            @person = exact
+            @person.current_address ||= @current_address
+            @person.current_address.update_attributes(params[:current_address])
+            @current_address = @person.current_address
+          else
+            @person.save
+            # add address
+            @person.current_address = @current_address
+          end
+          
+          # if this person doesn't have a region, default to the current user's region
+          @person.region ||= session[:sitrack_user].user.person.region
           @person.save
-          # add address
-          @person.current_address = @current_address
+          
+          # try to create a user row for the person.
+          username_conflict = User.find_by_username(@current_address.email)
+          @person.user = User.new(:username => @current_address.email, :createdOn => Time.now()) unless username_conflict
+          @application = HrSiApplication.new(:fk_personID => @person.id)
+          @application.sitrack_tracking = SitrackTracking.new()
+          @person.hr_si_applications << @application
+          
+          flash[:notice] = 'Person was successfully created.'
+          format.html { redirect_to :controller => 'profile', :action => 'index', :id => @application.id }
+        else
+          format.html { render :action => "new" }
         end
-        
-        # if this person doesn't have a region, default to the current user's region
-        @person.region ||= session[:sitrack_user].user.person.region
-        @person.save
-        
-        # try to create a user row for the person.
-        username_conflict = User.find_by_username(@current_address.email)
-        @person.user = User.new(:username => @current_address.email, :createdOn => Time.now()) unless username_conflict
-        @application = HrSiApplication.new(:fk_personID => @person.id)
-        @application.sitrack_tracking = SitrackTracking.new()
-        @person.hr_si_applications << @application
-        
-        flash[:notice] = 'Person was successfully created.'
-        format.html { redirect_to :controller => 'profile', :action => 'index', :id => @application.id }
-      else
-        format.html { render :action => "new" }
       end
+    else
+      redirect_to :action => :new
     end
   end
       
