92ad352f53953f13505cd11df010b18217211fc8
Josh Starcher <josh.starcher@gmail.com>
2010-12-07 15:10:33 -0500

    from db is a Date, not a Time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
   
   def self.formatted_date(value=nil)
     if value
-      time = value.class == Time ? value : Time.parse(value) 
+      time = value.class == Date ? value : Time.parse(value) 
     else 
       time = Time.now
     end
