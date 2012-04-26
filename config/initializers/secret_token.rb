fde800308f0bd15ca024011528b00eff20b42cf2
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-04-26 15:40:06 +0800

    WIP Upgrading to rails 3.2.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitrack::Application.config.secret_token = '761ace0d844653d8419dc6660106afe146b1fa972534cf5ac71f7bab36212124120bc5d0cf432f4f40d05d65a56484138d589abc78b65f0d582fd87833aea6ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+Sitrack::Application.config.session_store :cookie_store, key: '_sitrack_session', secret: '3475adcb1bbe8441964f9d9a45d094148664f0ec51e76d7b0ea40446ddd81e38320f757f80cd4e6b49079da52f747125'
+# config.action_controller.session = {
+#   :key => "_sitrack_session",
+#   :secret => "3475adcb1bbe8441964f9d9a45d094148664f0ec51e76d7b0ea40446ddd81e38320f757f80cd4e6b49079da52f747125"
+# }
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sitrack::Application.config.session_store :active_record_store
