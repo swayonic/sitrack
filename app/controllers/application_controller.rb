38506f8bee1f2619c731a08a1d521572fdc090c8
Josh Starcher <josh.starcher@gmail.com>
2012-08-01 16:48:13 -0400

    remove testing code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -61,7 +61,6 @@ class ApplicationController < ActionController::Base
   end
   
   def current_user
-    @current_user = User.find(286)
     if !@current_user && session[:user_id]
       @current_user = User.find(session[:user_id])
     end
