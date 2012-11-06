5fdda7a045151bb53c6d4cc58e1726da184c7ae3
Justin Sabelko <justin.sabelko@cojourners.com>
2012-11-05 18:49:43 -0600

    Use sendgrid for sending emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Sitrack::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.default_url_options = { :host => 'https://sitrack.uscm.org' }
   config.after_initialize do # Apparently Rails doesn't recognize 1.day during initialization. See: https://github.com/mperham/dalli/issues/243
